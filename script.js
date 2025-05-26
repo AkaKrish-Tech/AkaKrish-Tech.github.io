c7e2010d60256cbc05ef88a8d12fbe51edbc392f
AkaKrish <akashkrishnan1980@gmail.com>
2025-05-26 22:24:32 +0530

    new add

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ function toggleInfo(product) {
     if (product === "krish") {
       info.textContent = "Krish AI is an offline, multilingual AI assistant for Android & Windows, focusing on privacy and smart device control.";
     } else if (product === "gears") {
-      info.textContent = "Gears connects to microcontrollers like Arduino, detects components using AI, and generates real-time code for developers.";
+      info.textContent = "Gears is an student like ai that auto train itself on what you teach it in all aspects.";
+    } else if (product === "vairavasec") {
+      info.textContent = "Vairava Sec is an AI security softwre that provieds security to softwares , chatbot, ai tools.";
     }
   }
 }
